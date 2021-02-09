92233ff67112e04b3afb1fc5d17b0645d049864f
Felix Berg <felix.berg@hotmail.no>
2021-02-09 17:13:29 +0100

    ny start

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include<iostream>
+#include "std_lib_facilities.h"
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include "keyPressed.h"
 
-int main(){
-    std::cout << "Hello world";
-    int sadude = 47;
-    return 0;
+int main()
+{
+	bool spacePressed = false;
+	while (not spacePressed)
+	{
+		if(abs(keyPressed()) == 1){
+			cout << keyPressed() << endl;
+		} else if(keyPressed() == 2){
+			spacePressed = true;
+		}
+	}
+
+	keep_window_open();
 }
